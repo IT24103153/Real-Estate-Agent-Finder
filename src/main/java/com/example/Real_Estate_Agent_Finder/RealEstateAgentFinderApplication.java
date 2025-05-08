f29d7e30e8e9a22fb8f00ce335b7bb8bb95e1b6d
Himath <it24103153@my.sliit.lk>
2025-05-08 18:14:31 +0530

    initial project setup

---
+++ src/main/java/com/example/Real_Estate_Agent_Finder/RealEstateAgentFinderApplication.java
@@ -0,0 +1,13 @@
+package com.example.Real_Estate_Agent_Finder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealEstateAgentFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealEstateAgentFinderApplication.class, args);
+	}
+
+}
